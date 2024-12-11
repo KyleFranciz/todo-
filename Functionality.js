c0d2d73feb092d7dab3d8fc41d3e323b7d73c678
Kyle Francis <sjack421@gmail.com>
2024-12-11 02:27:47 -0500

    Add files via upload

--- Functionality.js
+++ Functionality.js
@@ -6,34 +6,34 @@
 // - Make the finished items disappear and move to completed section
 
 
-// The following code is useful for loading the JS functions into the page before the page loads 
- document.addEventListener('DOMContentLoaded', () => { // This part of the code is used for loading the script page before the rest of the other elements load.
+//^ Create the variables:
 
-    // List all of the core elements that I'll use from html and change them into JS variable:
-    // The reason why all of these are const, is because it will be constantly accessed through the 
+// Since we are working on with the input, the input will be one of the variables:
+const inputTask = document.getElementById("input-box");
 
-    // Add 'todo-form' add the parenthesis to be targeted and stored as a const variable
-    // Has do with controlling all the elements with in the un-ordered lit
-    const todoForm = document.getElementById('todo-form') // enter name of the id, that I want to use in the function 
+// Next is the list to access the ul list to use:
+const listContainer = document.getElementById("list-container")
+
+//^ Create a functions:
+// Create function for addTask with the button so that I can add the tasks to the list 
+function addTask() {
+    //if the inputTask value = nothing then alert the user:
+    if( inputTask.value === '') {
+        alert("Please enter a task to add to the list!")
+    } 
+    // If the input has something then make sure that the li gets the value from the input to
+    // append to the list element 
+    else { 
+        //make changeable variable, create element li:
+        let li = document.createElement("li");
+        // innerHtml helps when accessing the text on the inner part of the "li" Element
+        li.innerHTML = inputTask.value; // link the value of whatever is entered into the new html element
+        // Make "li" variable to get added underneath the Unordered Lists to be displayed 
+        listContainer.appendChild(li);
+    }
+    // Reset the inputTsk after the input is entered inp it
     
-    // Add 'input' as a JS variable to be used used in the project, since input is going to be essential to creating a new list element
-    const input = document.getElementById('input') // Store the input as a variable
 
-    // Add unordered list as another constant since the regular list is going to be added with in the Ul
-    const todoList = document.getElementById('todo-list')
+}
 
 
-    // Create a function that will avoid refreshing the page
-    todoForm.addEventListener('submit', function(event) {
-        event.preventDefault() //This function stops the page from reloading whenever the submit button is pressed 
-
-        //If There is no text entered, make an alert to let the user know to enter a task
-
-
-        //NOTE: When dealing with the value variable to 
-        // Get the value of whatever is typed in the input and clear the extra the white spaces with in the round
-    })
-
-
- });
-//? Declare the variables to use for this javascript feature:
