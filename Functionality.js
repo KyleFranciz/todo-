8f0d47c1bbf0ef0597e073569b91d374aaee21f7
Kyle Francis <sjack421@gmail.com>
2024-12-12 04:17:04 -0500

    Add files via upload

--- Functionality.js
+++ Functionality.js
@@ -12,10 +12,11 @@
 const inputTask = document.getElementById("input-box");
 
 // Next is the list to access the ul list to use:
-const listContainer = document.getElementById("list-container")
+const listContainer = document.getElementById('list-container')
+
 
 //^ Create a functions:
-// Create function for addTask with the button so that I can add the tasks to the list 
+//? Create function for addTask with the button so that I can add the tasks to the list 
 function addTask() {
     //if the inputTask value = nothing then alert the user:
     if( inputTask.value === '') {
@@ -25,14 +26,20 @@ function addTask() {
     // append to the list element 
     else { 
         //make changeable variable, create element li:
-        let li = document.createElement("li");
+        let li = document.createElement('li');
+
         // innerHtml helps when accessing the text on the inner part of the "li" Element
-        li.innerHTML = inputTask.value; // link the value of whatever is entered into the new html element
+        li.innerText = inputTask.value; // link the value of whatever is entered into the new html element
+
+        // Add the class 'todo-list' to the li element to get the styles created to be added each time a new element
+        // is created
+        li.classList.add('todo-list')
+
         // Make "li" variable to get added underneath the Unordered Lists to be displayed 
         listContainer.appendChild(li);
     }
-    // Reset the inputTsk after the input is entered inp it
     
+    // Reset the inputTask to clear after each task is added
 
 }
 
