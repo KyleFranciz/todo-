4c7786763160ba8e3e8ccb314ad3f2fa1f63b63b
Kyle Francis <sjack421@gmail.com>
2024-12-19 01:01:01 -0500

    Add files via upload

--- Functionality.js
+++ Functionality.js
@@ -1,2 +1,58 @@
+//ADD Functionality for the add task input section
+
+//Variables that will be used:
+const mainPage = document.getElementsByClassName('main-page')
+
+const centerBox = document.getElementById('center-section')
+
+const inputTask = document.getElementById('input-task')
 
 
+
+
+
+// Situational Variables
+//? NOTE: clickedAlready is used as a flag to check once a task is complete, so that a function
+//?         stops iterating over and over.
+var PhraseShown = false
+
+
+//Create a function that expands the height of the box a certain amount when the box is clicked:
+function expandCenterBox() {
+    //set the padding bottom style to 90px
+    centerBox.style.paddingBottom = '90px';
+}
+
+//Create a function to add a h2 tittle to the center section:
+function createIntroPhrase() {
+    if (!PhraseShown) {
+         //create a variable for the intro phrase
+        const introPhrase = document.createElement('h2')
+
+        //define the text within the intro phrase:
+        introPhrase.textContent = "Let's Add Some Tasks"
+
+        //add a class to the phrase to help give it some style
+        introPhrase.classList.add('intro-phrase')
+
+        //append the introPhrase to the centerBox:
+        //* centerBox.appendChild(introPhrase)
+
+        
+
+
+    }
+    
+}
+
+
+//Create an if statement:
+// If the input is selected then expand the center-box height:
+//? NOTE: addEventListener(event(click, input, change, useCapture))
+inputTask.addEventListener('click', function() {
+    //expand the center box function:
+    expandCenterBox();
+    //create the intro phrase after the initial activation:
+    createIntroPhrase();
+    
+});
