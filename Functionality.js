1b5ab01c89db1cae1b9c618301445e712960a007
Kyle Francis <sjack421@gmail.com>
2024-12-20 13:43:35 -0500

    Add files via upload

--- Functionality.js
+++ Functionality.js
@@ -1,39 +1,64 @@
 //ADD Functionality for the add task input section
 
-//Variables that will be used:
-const mainPage = document.getElementsByClassName('main-page')
+//Variables that will be used and some examples:
+//const mainPage = document.getElementsByClassName('main-page')
 
 const centerBox = document.getElementById('center-section')
 
-const inputTask = document.getElementById('input-task')
+const introPhrase = document.getElementById('introPhrase')
 
 
 
+//Create a function that makes the intro phrase invisible when the mouse is not on the centerBox
+function revealWord() {
+    introPhrase.classList.add('intro-phrase')
+}
+
+function hideWord() {
+    introPhrase.classList.remove('intro-phrase') 
+}
+
+centerBox.addEventListener('mouseover', revealWord)
+centerBox.addEventListener('mouseout', hideWord)
+//Create a function that removes an id when the input is selected:
+//function resetBox() {
+    //changes the id to another style I made that is identical but without the hover
+    //centerBox.id = 'center-section-regular'
+//}
+
+
+//Create a function that activates when mouse hovers over the center box:
+
+//centerBox.addEventListener('mouseenter', () => {
+    //expands the padding on the bottom by 90px
+    //centerBox.style.paddingBottom = '90px'
+    
+//}) 
 
 
 // Situational Variables
 //? NOTE: clickedAlready is used as a flag to check once a task is complete, so that a function
 //?         stops iterating over and over.
-var PhraseShown = false
+//var PhraseShown = false
 
 
 //Create a function that expands the height of the box a certain amount when the box is clicked:
-function expandCenterBox() {
+//function expandCenterBox() {
     //set the padding bottom style to 90px
-    centerBox.style.paddingBottom = '90px';
-}
+    //centerBox.style.paddingBottom = '90px';
+//}
 
 //Create a function to add a h2 tittle to the center section:
-function createIntroPhrase() {
-    if (!PhraseShown) {
+//function createIntroPhrase() {
+    //if (!PhraseShown) {
          //create a variable for the intro phrase
-        const introPhrase = document.createElement('h2')
+        //const introPhrase = document.createElement('h2')
 
         //define the text within the intro phrase:
-        introPhrase.textContent = "Let's Add Some Tasks"
+        //introPhrase.textContent = "Let's Add Some Tasks"
 
         //add a class to the phrase to help give it some style
-        introPhrase.classList.add('intro-phrase')
+        //introPhrase.classList.add('intro-phrase')
 
         //append the introPhrase to the centerBox:
         //* centerBox.appendChild(introPhrase)
@@ -41,18 +66,18 @@ function createIntroPhrase() {
         
 
 
-    }
+    //}
     
-}
+//}
 
 
 //Create an if statement:
 // If the input is selected then expand the center-box height:
 //? NOTE: addEventListener(event(click, input, change, useCapture))
-inputTask.addEventListener('click', function() {
+//inputTask.addEventListener('click', function() {
     //expand the center box function:
-    expandCenterBox();
+    //expandCenterBox();
     //create the intro phrase after the initial activation:
-    createIntroPhrase();
+    //createIntroPhrase();
     
-});
+//});
